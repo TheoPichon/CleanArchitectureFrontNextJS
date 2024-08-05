28498bf5cf9f75ead2eea261e1f2750a3bf0b49a
TheoPichon <129216620+TheoPichon@users.noreply.github.com>
2024-08-05 17:34:55 +0200

    feat(meal): ui page meal

--- src/modules/order/core/model/ordering.domain-model.ts
+++ src/modules/order/core/model/ordering.domain-model.ts
@@ -5,11 +5,33 @@ export namespace OrderingDomainModel {
     tableId: string | null;
   };
 
+  export enum MealType {
+    ENTRY = 'entry',
+    MAIN_COURSE = 'mainCourse',
+    DESSERT = 'dessert',
+    DRINK = 'drink',
+  }
+
+  export type MealId = string;
+
+  export type Meal = {
+    id: MealId;
+    title: string;
+    type: MealType;
+    requiredAge: number | null;
+  };
+
   export type Guest = {
     id: string;
     firstName: string;
     lastName: string;
     age: number;
+    meals: {
+      entry: MealId | null;
+      mainCourse: MealId | null;
+      dessert: MealId | null;
+      drink: MealId | null;
+    };
   };
 
   export enum Step {
